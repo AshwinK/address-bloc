cafce7e89478388b668e99c815aeb974bc6afc53
Ashwin Khurana <ashwin@ashwinkhurana.com>
2017-02-24 08:59:14 -0800

    assignment-6-menu: complete

--- controllers/menu_controller.rb
+++ controllers/menu_controller.rb
@@ -13,94 +13,116 @@ class MenuController
     puts "2 - Create an entry"
     puts "3 - Search for an entry"
     puts "4 - Import entries from a CSV"
-    puts "5 - Exit"
+    puts "5 - View Entry Number n"
+    puts "6 - Exit"
     print "Enter your selection: "
 
     selection = gets.to_i
     puts "You picked #{selection}"
 
     case selection
-      when 1
-        system "clear"
-        view_all_entries
-        main_menu
-      when 2
-        system "clear"
-        create_entry
-        main_menu
-      when 3
-        system "clear"
-        search_entries
-        main_menu
-      when 4
-        system "clear"
-        read_csv
-        main_menu
-      when 5
-        puts "Good-bye!"
-        exit(0)
-
-      else
-        system "clear"
-        puts "Sorry, that is not a valid input"
-        main_menu
-      end
-    end
-
-    def view_all_entries
-      address_book.entries.each do |entry|
-        system "clear"
-        puts entry.to_s
-
-        entry_submenu(entry)
-      end
-
+    when 1
       system "clear"
-      puts "End of entries"
-    end
-
-    def create_entry
+      view_all_entries
+      main_menu
+    when 2
       system "clear"
-      puts "New AddressBloc Entry"
-
-      print "Name: "
-      name = gets.chomp
-      print "Phone number: "
-      phone = gets.chomp
-      print "Email: "
-      email = gets.chomp
-
-      address_book.add_entry(name, phone, email)
-
+      create_entry
+      main_menu
+    when 3
       system "clear"
-      puts "New entry created"
+      search_entries
+      main_menu
+    when 4
+      system "clear"
+      read_csv
+      main_menu
+    when 5
+      system "clear"
+      entry_n_submenu
+      main_menu
+    when 6
+      puts "Good-bye!"
+      exit(0)
+
+    else
+      system "clear"
+      puts "Sorry, that is not a valid input"
+      main_menu
+    end
+  end
+
+  def entry_n_submenu
+    print "What entry would you like to see?\n"
+    selection = gets.chomp.to_i
+
+    if address_book.entries.count == 0
+      puts "You have no entries. Press enter to go back to the main menu"
+      main_menu
     end
 
-    def search_entries
+    if selection < address_book.entries.count
+      puts address_book.entries[selection]
+      puts "Press enter to go back to the main menu"
+      gets.chomp
+      system "clear"
+    else
+      puts "#{selection} is not valid"
+      entry_n_submenu
+    end
+  end
+
+  def view_all_entries
+    address_book.entries.each do |entry|
+      system "clear"
+      puts entry.to_s
+
+      entry_submenu(entry)
     end
 
-    def read_csv
-    end
+    system "clear"
+    puts "End of entries"
+  end
 
-    def entry_submenu(entry)
-      puts "n - next entry"
-      puts "d - delete entry"
-      puts "e - edit this entry"
-      puts "m - return to main menu"
+  def create_entry
+    system "clear"
+    puts "New AddressBloc Entry"
 
-      selection = gets.chomp
+    print "Name: "
+    name = gets.chomp
+    print "Phone number: "
+    phone = gets.chomp
+    print "Email: "
+    email = gets.chomp
 
-      case selection
-        when "n"
-        when "d"
-        when "e"
-        when "m"
-          system "clear"
-          main_menu
-        else
-          system "clear"
-          puts "#{selection} is not a valid input"
-          entry_submenu(entry)
+    address_book.add_entry(name, phone, email)
+
+    system "clear"
+    puts "New entry created"
+  end
+
+  def search_entries
+  end
+
+  def read_csv
+  end
+
+  def entry_submenu(entry)
+    puts "n - next entry"
+    puts "d - delete entry"
+    puts "e - edit this entry"
+    puts "m - return to main menu"
+
+    selection = gets.chomp
+
+    case selection
+    when "n", "d", "e", "m"
+      system "clear"
+      main_menu
+    else
+      system "clear"
+      puts "#{selection} is not a valid input"
+      entry_submenu(entry)
     end
   end
 end
